61708ae4ae7d8ae47d4e173605535e28f779e668
Devify <122911952+Devi-fy@users.noreply.github.com>
2023-05-16 17:20:06 +0000

    Update

--- Project-2/script.js
+++ Project-2/script.js
@@ -1,27 +1,27 @@
-// Send message to Discord webhook
-function sendToDiscord(message) {
-    fetch("https://discord.com/api/webhooks/1108072923618607235/1iJXs-mGDZ_Er81qZbt_Nhwm7kqXoftPLhTkvvut7ntumrB884hcTPUD51wvKZmUIiko", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ content: message })
-    })
-    .then(response => {
-      if (response.ok) {
-        console.log("Message sent to Discord!");
-      } else {
-        console.log("Failed to send message to Discord.");
-      }
-    })
-    .catch(error => {
-      console.log("Error:", error);
-    });
-  }
-  
-  // Example usage
-  const button = document.querySelector(".fancy-button");
-  button.addEventListener("click", function() {
-    sendToDiscord("Button clicked!");
-  });
-  
+document.getElementById('bugReportForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+    
+    // Get the bug title and description values
+    var bugTitle = document.getElementById('bugTitle').value;
+    var bugDescription = document.getElementById('bugDescription').value;
+    
+    // Prepare the bug report payload
+    var payload = {
+        content: '**Bug Report**\n\n' +
+                 '**Title:** ' + bugTitle + '\n\n' +
+                 '**Description:** ' + bugDescription
+    };
+    
+    // Send the bug report to the webhook
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://discord.com/api/webhooks/1108072923618607235/1iJXs-mGDZ_Er81qZbt_Nhwm7kqXoftPLhTkvvut7ntumrB884hcTPUD51wvKZmUIiko', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(payload));
+    
+    // Clear the form fields
+    document.getElementById('bugTitle').value = '';
+    document.getElementById('bugDescription').value = '';
+    
+    // Provide visual feedback to the user
+    alert('Bug report submitted successfully!');
+});
